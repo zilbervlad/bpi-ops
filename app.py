1582828fcf3fd2b1dd669ce40e6fbfe2e432323f
vladislav zilber <vladislavzilber@Mac.ht.home>
2026-03-14 14:19:52 -0400

    Add create-db route

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
+    @app.route("/create-db")
+def create_db():
+    from app.extensions import db
+    db.create_all()
+    return "Database tables created"

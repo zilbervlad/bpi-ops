31201bda1da63c5e7520da3dbe2ced77ef659047
vladislav zilber <vladislavzilber@Mac.ht.home>
2026-03-14 14:25:47 -0400

    Fix create-db route placement

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from app import create_app
+from app.extensions import db
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
-    @app.route("/create-db")
+
+@app.route("/create-db")
 def create_db():
-    from app.extensions import db
     db.create_all()
     return "Database tables created"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
